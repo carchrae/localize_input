597e33bb42a3b0fd58c3ea7d4f5a208c2911b43d
Tom Carchrae <tom@intellecti.ca>
2017-05-05 14:30:30 -0700

    only apply to strings (match doesn't work elsewhere)

--- lib/localize_input.rb
+++ lib/localize_input.rb
@@ -11,11 +11,13 @@ module LocalizeInput
       attr_names.flatten.each do |attr|
         define_method "#{attr}=" do |input|
           begin
-            Rails.logger.debug "Input: #{input.inspect}"
-            separator = I18n.t("separator", :scope => "number.format")
-            delimiter = I18n.t("delimiter", :scope => "number.format")
-            input.gsub!(delimiter, "") if input.match(/\d+#{Regexp.escape(delimiter)}+\d+#{Regexp.escape(separator)}+\d+/) # Remove delimiter
-            input.gsub!(separator, ".") # Replace separator with db compatible character
+            if input.is_a? String
+              Rails.logger.debug "Input: #{input.inspect}"
+              separator = I18n.t("separator", :scope => "number.format")
+              delimiter = I18n.t("delimiter", :scope => "number.format")
+              input.gsub!(delimiter, "") if input.match(/\d+#{Regexp.escape(delimiter)}+\d+#{Regexp.escape(separator)}+\d+/) # Remove delimiter
+              input.gsub!(separator, ".") # Replace separator with db compatible character
+            end
             self[attr] = input
           rescue
             Rails.logger.warn "Can't localize input: #{input}"
